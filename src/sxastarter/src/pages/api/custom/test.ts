0fecb6a199e49d5e5de152f8ed7cb652ef8599d3
Yevhenii Sosidko <40215123+NoCodingNoLifee@users.noreply.github.com>
2024-11-12 15:05:21 +0200

    upgrade to the last version

--- src/sxastarter/src/pages/api/custom/test.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-const TestApi = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  return res.status(200).send("Custom Endpoint works");
-};
-
-export default TestApi;
